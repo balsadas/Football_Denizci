778072edb589065fef422d19a8b968a779485c5d
Developer <balsadas>
2023-06-03 17:38:20 +0500

    sd

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div style={{backgroundImage: "url('./img/football1.jpg')", width: "100%",  height:'45vw', backgroundPosition:'center', backgroundSize:'cover', backgroundRepeat: "no-repeat"}}>
+      ewf
+    </div>
+  )
+}
+
+export default Navbar
