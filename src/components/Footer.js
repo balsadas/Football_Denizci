512886145cab78e893b2664a6ddeee94b2adf601
Developer <balsadas>
2023-06-22 11:43:29 +0500

    dyndy

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='flex md:text-[1.1vw] font-[400] text-[3vw] border-t border-green-300 bg-[#f5f7f7e8] justify-center p-3'>
+            Powered by: <span className='font-[600] pl-2'>Pikir</span>
+        </div>
+    )
+}
+
+export default Footer
--- src/page/Home.js
+++ src/page/Home.js
@@ -6,6 +6,7 @@ import Teams from '../components/Teams'
 import Gallery from '../components/Gallery'
 import { useState } from 'react'
 import Our_Priorities from '../components/Our_Priorities'
+import Footer from '../components/Footer'
 
 function Home() {
 
@@ -15,16 +16,17 @@ function Home() {
       <Navbar />
       <AboutUs />
       <Teams />
-      <Our_Priorities/>
+      <Our_Priorities />
       <Gallery />
       <ContactUs />
 
-          <div className=' fixed bottom-8 right-4 z-[98]'>
-            <a href='#'>
-              <img src='./img/up-arrow.png' className='md:w-[3vw] md:h-[3vw] w-[8vw] h-[8vw] ' />
-            </a>
-          </div>
 
+      <div className=' fixed bottom-8 right-4 z-[98]'>
+        <a href='#'>
+          <img src='./img/up-arrow.png' className='md:w-[3vw] md:h-[3vw] w-[8vw] h-[8vw] ' />
+        </a>
+      </div>
+      <Footer />
     </div>
   )
 }
